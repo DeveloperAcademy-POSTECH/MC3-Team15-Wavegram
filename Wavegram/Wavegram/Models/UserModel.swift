ed6ecf89eac151c813b0a0f259137190d539c0d2
insub4067 <vb15@kakao.com>
2022-07-18 17:28:25 +0900

    [Feat] JsonManager
    - JsonFile이 추가되었습니다
    
    - Json을 읽어오는 Manager를 추가 하였습니다
    
    - User, Feed Struct files를 추가하였습니다
    
    - Folder 구조를 다시 정리하였습니다

---
+++ Wavegram/Wavegram/Models/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  Wavegram
+//
+//  Created by Kim Insub on 2022/07/18.
+//
+
+import Foundation
+
+struct User: Hashable, Codable {
+    
+    let name: String
+    let profileImage: String
+}
