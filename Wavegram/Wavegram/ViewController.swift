46f9ac15cebae781a5abde3d7780759882cf42df
김상현 <kshy1019@naver.com>
2022-07-18 15:27:01 +0900

    init xcode project

---
+++ Wavegram/Wavegram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wavegram
+//
+//  Created by 김상현 on 2022/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
