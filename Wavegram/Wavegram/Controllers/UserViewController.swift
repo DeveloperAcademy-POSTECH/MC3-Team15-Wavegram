c406a3bd99a0d2007edd6b25d3575c7c93e768d8
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-23 08:47:24 +0900

    CHORE: 뷰 컨트롤러 프리뷰 추가
    - 뷰 컨트롤러에 프리뷰를 추가하였습니다.
    - 코딩하실 때 화면이 좁아서 잘 안 보인다면 주석 처리를 하시고 작업해 주세요.

--- Wavegram/Wavegram/Controllers/UserViewController.swift
+++ Wavegram/Wavegram/Controllers/UserViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 final class UserViewController: UIViewController {
     
@@ -344,3 +345,31 @@ extension UserViewController: UICollectionViewDelegate, UICollectionViewDataSour
         navigationController?.pushViewController(vc, animated: true)
     }
 }
+
+
+// MARK: SwiftUI - Preview 추가
+struct UserViewControllerPreView: PreviewProvider {
+    static var previews: some View {
+        UserViewController().userViewControllerToPreview()
+    }
+}
+
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+
+    func userViewControllerToPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
+#endif
