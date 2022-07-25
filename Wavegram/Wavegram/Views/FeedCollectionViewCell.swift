5ac85268ae82039244e62ad8fb12a6e4343702a0
EunJI <83203198+ejalice@users.noreply.github.com>
2022-07-26 00:32:04 +0900

    [Feat] FeedCollelctionViewCell
    Add Profile Image

--- Wavegram/Wavegram/Views/FeedCollectionViewCell.swift
+++ Wavegram/Wavegram/Views/FeedCollectionViewCell.swift
@@ -10,13 +10,54 @@ import AVFoundation
 
 class FeedCollectionViewCell: UICollectionViewCell {
     
+    var feeds: Feed?
+    
+    // Profile Image
+    // TODO: UIIMageView -> UIButton
+    private let profileImage: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 21
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        addSubview(profileImage)
     }
     
     // Basic Setting
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    // Configure
+    public func configure(with model: Feed) {
+        self.feeds = model
+        profileImage.image = UIImage(named: model.owner.profileImage)
+        
+        applyConstraints(model)
+    }
+    
+    // Constraints
+    private func applyConstraints(_ model: Feed) {
+        let r = frame.width * 21.0 / 175.0 // ProfileImage 반지름
+        
+        let profileImageConstraints = [
+            profileImage.topAnchor.constraint(equalTo: topAnchor),
+            profileImage.leadingAnchor.constraint(equalTo: leadingAnchor),
+            // 42: 350 = 21:175
+            profileImage.heightAnchor.constraint(equalToConstant: r),
+            profileImage.widthAnchor.constraint(equalToConstant: r)
+        ]
+        
+        NSLayoutConstraint.activate(profileImageConstraints)
+        
     }
     
 }
