22f0556107c56d7e14494cd08d630a535e9f7b93
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-26 19:47:28 +0900

    Fix: 스펙트로그램 실행 오류 픽스
    뷰 컨트롤러 코드 추가로 해결했습니다.

--- Wavegram/Wavegram/Controllers/MainTabBarViewController.swift
+++ Wavegram/Wavegram/Controllers/MainTabBarViewController.swift
@@ -15,7 +15,7 @@ class MainTabBarViewController: UITabBarController {
         
         let vc1 = UINavigationController(rootViewController: HomeViewController())
         let vc2 = UINavigationController(rootViewController: SeacrhViewController())
-        let vc3 = UINavigationController(rootViewController: UserViewController())
+        let vc3 = UINavigationController(rootViewController: SpectrogramViewController())
         
         vc1.tabBarItem.image = UIImage(systemName: "music.note.house.fill")
         vc2.tabBarItem.image = UIImage(systemName: "magnifyingglass")
--- Wavegram/Wavegram/Extension/SpectrogramExtension.swift
+++ Wavegram/Wavegram/Extension/SpectrogramExtension.swift
@@ -1,20 +1,15 @@
 //
-//  SpectrogramExtension.swift
-//  Wavegram
+//  AVCaptureAudioDataOutputSampleBufferDelegate.swift
+//  Spectrogram
 //
 //  Created by 김제필 on 7/18/22.
 //
 
-
-// MARK: AVCaptureAudioDataOutputSampleBufferDelegate + AVFoundation Support를 위한 코드
-
+import Foundation
 import AVFoundation
 
+// MARK: AVCaptureAudioDataOutputSampleBufferDelegate + AVFoundation Support
 
-/*
- @method captureOutput:didOutputSampleBuffer:fromConnection:
- @abstract Called whenever an AVCaptureAudioDataOutput instance outputs a new audio sample buffer.
- */
 extension Spectrogram: AVCaptureAudioDataOutputSampleBufferDelegate {
 
     // MARK: 오디오 캡쳐 정의
@@ -22,12 +17,9 @@ extension Spectrogram: AVCaptureAudioDataOutputSampleBufferDelegate {
 
     // 관련 문서: https://developer.apple.com/documentation/avfoundation/avcapturedevice
     // captureOutput(_:didOutput:from:) 함수의 콜이 필요함.
-    public func captureOutput(
-        _ output: AVCaptureOutput,
-        didOutput sampleBuffer: CMSampleBuffer,
-        from connection: AVCaptureConnection
-    ) {
-
+    public func captureOutput(_ output: AVCaptureOutput,
+                              didOutput sampleBuffer: CMSampleBuffer,
+                              from connection: AVCaptureConnection) {
         // 보유하고 있는 블록 버퍼로 오디오 버퍼 목록 가져오기
         // MARK: Code block Starts here --->
         var audioBufferList = AudioBufferList()
@@ -41,8 +33,7 @@ extension Spectrogram: AVCaptureAudioDataOutputSampleBufferDelegate {
             blockBufferAllocator: nil,
             blockBufferMemoryAllocator: nil,
             flags: kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment,
-            blockBufferOut: &blockBuffer
-        )
+            blockBufferOut: &blockBuffer)
 
         guard let data = audioBufferList.mBuffers.mData else {
             // return: raw 오디오 데이터에 대한 포인터
@@ -50,28 +41,19 @@ extension Spectrogram: AVCaptureAudioDataOutputSampleBufferDelegate {
         }
         // MARK: Code block ends here <---
 
-        
         /*
          스펙트로그램을 그리려면 정확한 sampleCount(여기서는 1024로 정의) 샘플이 필요하지만
          AVFoundation의 오디오 샘플 버퍼에 항상 정확한 1024개 샘플이 포함되어 있지 않을 수 있다.
          따라서 각각의 오디오 샘플 버퍼를 rawAudioData에 추가해서 이런 케이스를 처리한다.
          */
         if self.rawAudioData.count < Spectrogram.sampleCount * 2 {
-            // MARK: ---> 데이터에서 배열 생성 -> rawAudioData에 추가
             let actualSampleCount = CMSampleBufferGetNumSamples(sampleBuffer)
 
-            let ptr = data.bindMemory(
-                to: Int16.self,
-                capacity: actualSampleCount
-            )
-            let buf = UnsafeBufferPointer(
-                start: ptr,
-                count: actualSampleCount)
+            let ptr = data.bindMemory(to: Int16.self, capacity: actualSampleCount)
+            let buf = UnsafeBufferPointer(start: ptr, count: actualSampleCount)
 
             rawAudioData.append(contentsOf: Array(buf))
-            // MARK: <---
         }
-
 
         // rawAudioData 배열에 데이터 추가
         //      1. -> raw 오디오 데이터 중 첫 번째 sampleCount가 processData(values: ) 함수로 넘겨짐
@@ -87,63 +69,56 @@ extension Spectrogram: AVCaptureAudioDataOutputSampleBufferDelegate {
     }
 
 
-    // MARK: 오디오 캡쳐를 위한 환경설정(Configuration): AVCaptureSession 인스턴스 설정
+    // MARK: 사운드 캡쳐를 위한 환경설정(Configuration): AVCaptureSession 인스턴스 설정
     func configureCaptureSession() {
         switch AVCaptureDevice.authorizationStatus(for: .audio) {
             case .authorized:
                     break
             case .notDetermined:
                 sessionQueue.suspend()
-                AVCaptureDevice.requestAccess(
-                    for: .audio,
-                    completionHandler: { granted in
-                        if !granted {
-                            fatalError("App requires microphone access.")
-                        } else {
-                            self.configureCaptureSession()
-                            self.sessionQueue.resume()
-                        }
-                    })
-            return
-        default:
-            fatalError("App requires microphone access.")
+                AVCaptureDevice.requestAccess(for: .audio,
+                                              completionHandler: { granted in
+                    if !granted {
+                        fatalError("App requires microphone access.")
+                    } else {
+                        self.configureCaptureSession()
+                        self.sessionQueue.resume()
+                    }
+                })
+                return
+            default:
+                fatalError("App requires microphone access.")
         }
 
 
-        // MARK: AVCaptureSession() 인스턴스의 위치를 마크하기 위한 코드
+        // MARK: AVCaptureSession 인스턴스의 위치를 마크하기 위한 코드
         // AVCaptureSession은 beginConfiguration() 과 commitConfiguration() 사이에 위치함.
 
-        // MARK: Code block starts here --->
+        // MARK: 하단의 commitConfiguration()과 한 페어를 이룸
         captureSession.beginConfiguration()
 
         if captureSession.canAddOutput(audioOutput) {
             captureSession.addOutput(audioOutput)
         } else {
-            fatalError("Cannot add an audioOutput.")
+            fatalError("Can't add `audioOutput`.")
         }
 
-
-        // 빌트-인 마이크 정의
+        // 세션 캡쳐를 위해 아이폰의 빌트-인 마이크 정의
         guard
-            let microphone = AVCaptureDevice.default(
-                .builtInMicrophone,
-                for: .audio,
-                position: .unspecified
-            ),
+            let microphone = AVCaptureDevice.default(.builtInMicrophone,
+                                                     for: .audio,
+                                                     position: .unspecified),
             let microphoneInput = try? AVCaptureDeviceInput(device: microphone) else {
-                fatalError("Caanot create the microphone.")
+                fatalError("Can't create microphone.")
         }
 
         if captureSession.canAddInput(microphoneInput) {
             captureSession.addInput(microphoneInput)
-        } else {
-            fatalError("Cannot add an audioInput.")
         }
 
-        // MARK: Code block ends here <---
+        // MARK: 윗단의 beginConfiguration()과 페어를 이룸
         captureSession.commitConfiguration()
     }
-
 
     // spectrogram 시작
     func startRunning() {
