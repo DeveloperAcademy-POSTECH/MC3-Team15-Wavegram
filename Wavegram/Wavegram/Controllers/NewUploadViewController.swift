aab1f2a113f612e9e7251563141da2c0ff9101ae
김상현 <kshy1019@naver.com>
2022-07-29 20:54:24 +0900

    [Feat] #64 키보드 크기 맞춰 화면 올리고 내리는 메서드 추가

--- Wavegram/Wavegram/Controllers/NewUploadViewController.swift
+++ Wavegram/Wavegram/Controllers/NewUploadViewController.swift
@@ -70,6 +70,14 @@ class NewUploadViewController: UIViewController {
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.addKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.removeKeyboardNotifications()
+    }
+    
     private let memoTextLengthLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -313,6 +321,16 @@ class NewUploadViewController: UIViewController {
          playToggleButtonConstraints].forEach{ NSLayoutConstraint.activate($0) }
     }
 
+    private func addKeyboardNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification , object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    private func removeKeyboardNotifications(){
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification , object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
     // MARK: OnTapGesture
     @objc func onTapRepresentativeImage(_ sender: UITapGestureRecognizer) {
         let alert =  UIAlertController(title: "사진을 골라주세요", message: "", preferredStyle: .actionSheet)
@@ -334,6 +352,22 @@ class NewUploadViewController: UIViewController {
 
     @objc func onTapRightBarButtonItem() {
         print("onTapRightBarButtonItem")
+    }
+    
+    @objc func keyboardWillShow(_ noti: NSNotification){
+        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            self.view.frame.origin.y -= keyboardHeight
+        }
+    }
+
+    @objc func keyboardWillHide(_ noti: NSNotification){
+        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            self.view.frame.origin.y += keyboardHeight
+        }
     }
 }
 
