0327b5de3803a4e2326fd0206f3e5a15edf39c93
EunJI <83203198+ejalice@users.noreply.github.com>
2022-07-29 15:03:05 +0900

    [Feat] HomeFeedCollectionViewCell
    Connect MainPage and ModifyPostViewController

--- Wavegram/Wavegram/Views/HomeFeedCollectionViewCell.swift
+++ Wavegram/Wavegram/Views/HomeFeedCollectionViewCell.swift
@@ -90,11 +90,13 @@ class HomeFeedCollectionViewCell: UICollectionViewCell {
         let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
         let modify = UIAlertAction(title: "수정하기", style: .default) { action in
-            // let vc = ModifyPost()
-            // self.present(vc)
+            let vc = ModifyPostViewController()
+            vc.modalPresentationStyle = .fullScreen
+            self.viewController?.present(vc, animated: true)
             print("수정하기")
         }
         let delete = UIAlertAction(title: "삭제하기", style: .destructive) { action in
+            // TODO: 게시물 삭제하기
             print("삭제하기")
         }
         let cancel = UIAlertAction(title: "취소하기", style: .cancel) { action in
