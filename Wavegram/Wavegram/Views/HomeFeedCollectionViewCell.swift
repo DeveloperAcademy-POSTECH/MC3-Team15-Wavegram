44c34236c7d5122bc2075a5dec58e9371f7e2da9
EunJI <83203198+ejalice@users.noreply.github.com>
2022-08-01 15:33:14 +0900

    [Feat] #88 View 연결
    ContributionUploadViewController와 HomeFeedCollectionVIewCell 연결

--- Wavegram/Wavegram/Views/HomeFeedCollectionViewCell.swift
+++ Wavegram/Wavegram/Views/HomeFeedCollectionViewCell.swift
@@ -78,7 +78,8 @@ class HomeFeedCollectionViewCell: UICollectionViewCell {
     
     // Contribute Button
     @objc func uploadContributeFeed() {
-        // TODO: Move to UploadContributeFeed
+        let vc = ContributionUploadViewController()
+        self.viewController?.navigationController?.pushViewController(vc, animated: false)
         print("Move to uploadContributeFeed View")
     }
     
@@ -90,7 +91,6 @@ class HomeFeedCollectionViewCell: UICollectionViewCell {
         
         let modify = UIAlertAction(title: "수정하기", style: .default) { action in
             let vc = ModifyPostViewController()
-            vc.modalPresentationStyle = .fullScreen
             self.viewController?.navigationController?.pushViewController(vc, animated: true)
             print("수정하기")
         }
